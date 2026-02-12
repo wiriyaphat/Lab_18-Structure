65ff8e133f5ffaf4db2e18b12d399e43e9cab4ab
Thiw <wiriyaphat6789@gmail.com>
2026-02-12 22:31:39 +0700

    Lab18_2 complete

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
+#include<algorithm>
+#include<utility>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *A, Rect *B){
+    pair<double, double> result = minmax((*A).x, (*B).x);
+    pair<double, double> result1 = minmax(((*A).x+(*A).w), ((*B).x+(*B).w));
+    pair<double, double> result2 = minmax((*A).y, (*B).y);
+    pair<double, double> result3 = minmax(((*A).y-(*A).h), ((*B).y-(*B).h));
+    
+    double Kwang=0, Yaw=0;
+    if(((*A).x+(*A).w) > (*B).x)
+    {
+        Kwang += (result1.first - result.second);
+        Yaw += (abs(result2.first) + abs(result3.second));
+        return Kwang*Yaw;
+    }
+    else{return 0;}
 }
